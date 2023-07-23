8f27f26a80bf96168b3905d42ff30b9ff371884c
ardi.widyanto <ardi.widyanto@bluebirdgroup.com>
2023-07-23 19:06:34 +0700

    update icon position on sidebar

--- src/components/Sidebar/sidebar-item.tsx
+++ src/components/Sidebar/sidebar-item.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const SidebarItem: React.FC<Props> = ({ href, isActive, text, icon}) => {
   return (
-    <Link href={href} className={classNames("flex items-center text-gray-900 rounded-lg hover:bg-gray-100 group p-2 active:rounded-lg space-x-2", isActive && "bg-gray-100")}>
+    <Link href={href} className={classNames("flex items-center text-gray-900 rounded-lg hover:bg-gray-100 group p-2 active:rounded-lg space-x-2 justify-center sm:justify-start", isActive && "bg-gray-100")}>
       {icon}
       <Typography className="md:inline-flex hidden" variant="subtitle" color="primary">{text}</Typography>
     </Link>
--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -8,7 +8,7 @@ import { SidebarItem } from ".";
 const Sidebar = () => {
   const pathname = usePathname();
   return (
-    <div className="w-1/6 space-y-3 shadow p-1 sm:p-4 h-full bg-white">
+    <div className="w-1/6 space-y-3 shadow p-1 sm:p-4 h-full bg-white rounded-lg">
       <Typography variant="title" className="sm:block hidden">Navbar</Typography>
       <SidebarItem href="/" isActive={pathname === "/"} text="Product" icon={<ProductIcon />} />
       <SidebarItem href="/cart" isActive={pathname === "/cart"} text="Cart" icon={<CartIcon />} />
